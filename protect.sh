5a8009437116d83a94cb51bc33f783e855fe41b7
Ilyass Saadi <isaadi@e1r7p6.1337.ma>
2022-05-07 16:26:40 +0000

    check if file exists before trying to copy it's contents

--- protect.sh
+++ protect.sh
@@ -10,7 +10,10 @@ else
 	filename="${filename%.*}"
 	def=__$(echo "$filename" | tr '[:lower:]' '[:upper:]')_$(echo "$extension" | tr '[:lower:]' '[:upper:]')__
 	echo -e "#ifndef $def\n# define $def\n" > /tmp/temptemp
-	cat "$1" >> /tmp/temptemp
+	if [ -f "$1" ];
+	then
+		cat "$1" >> /tmp/temptemp
+	fi
 	echo -e "\n#endif" >> /tmp/temptemp
 	cp /tmp/temptemp "$1"
 fi
