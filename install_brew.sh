f7b9dd8e7d9405b4831bcaaeeed16cf858284d94
Sick Leviathan <59098347+sickl8@users.noreply.github.com>
2022-06-27 18:39:07 +0100

    Update install_brew.sh

--- install_brew.sh
+++ install_brew.sh
@@ -1,2 +1,8 @@
-rm -rf ~/.brew /goinfre/$USER/.brew
-git clone --depth=1 https://github.com/Homebrew/brew /goinfre/$USER/.brew && ln -s /goinfre/$USER/.brew /Users/$USER/.brew && export PATH=$HOME/.brew/bin:$PATH && brew update && echo "export PATH=$HOME/.brew/bin:$PATH" >> ~/.bashrc
+if [ "$USER" == "" ]
+then
+	echo the USER environment variable is not set, export USER=your_login
+else
+	echo installing...
+	rm -rf ~/.brew /goinfre/$USER/.brew
+	git clone --depth=1 https://github.com/Homebrew/brew /goinfre/$USER/.brew && ln -s /goinfre/$USER/.brew /Users/$USER/.brew && $HOME/.brew/bin/brew update && echo "export PATH=$HOME/.brew/bin:$PATH" >> ~/.bashrc && echo "export PATH=$HOME/.brew/bin:$PATH" >> ~/.zshrc
+fi
