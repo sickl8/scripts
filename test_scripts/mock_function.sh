636c876e110bc634ef20bef6a67af22123991678
isaadi <isaadi@kinova.ca>
2022-09-22 20:19:12 +0100

    fixed a few issues with mock_function.sh

--- test_scripts/mock_function.sh
+++ test_scripts/mock_function.sh
@@ -6,14 +6,15 @@ else
 	do
 		if [ "$(grep $1 $file)" ]
 		then
-			proto=$(cat $file | regex.py '(^\s*(?:(?:inline|static)\s+){0,2}(?!else|typedef|return)\n*\w+\n*\s+\*?\s*('$1')\s*\([^0;{]+\)\s*\{)' -)
+			proto=$(cat $file | regex.py '(^\s*(?:(?:inline|static)\s+){0,2}(?!else|typedef|return)\n*\w+\n*\s*\**\s*('$1')\s*\([^0;{]+\)\s*\{)' -)
+			# proto=$(cat $file | regex.py '(^\s*(?:(?:inline|static)\s+){0,2}(?!else|typedef|return)\n*\w+\n*\s+\*?\s*('$1')\s*\([^0;{]+\)\s*\{)' -)
 			if [ "$proto" ]
 			then
 				echo "$file:";
 				mock_file=$(create_mock_file.sh "$file");
 				if [ "$mock_file" ]
 				then
-					return_type=$(cat $file | regex.py '^\s*(?:(?:inline|static)\s+){0,2}(?!else|typedef|return)\n*(\w+)\n*\s+\*?\s*('$1')\s*\([^0;{]+\)\s*\{' -)
+					return_type=$(cat $file | regex.py '^\s*(?:(?:inline|static)\s+){0,2}(?!else|typedef|return)\n*(\w+)\n*\s+\**\s*('$1')\s*\([^0;{]+\)\s*\{' -)
 					# echo "file=$file"
 					# echo "rt=$return_type"
 					wrap_proto=$(echo "$proto" | sed 's/'"$1"'/'__wrap_"$1"'/g')
