c2d3026fe548e09ed6fc6ee2ce118eb0e5ac59e3
aeghemari <aeghemari@kinova.ca>
2022-09-22 19:05:15 +0100

    added new testing functionalities

---
+++ test_scripts/create_test_file.sh
@@ -0,0 +1,33 @@
+if [ -z "$1" ]
+then
+    echo "$(basename -- $0): No argument supplied"
+else
+	filename=$(basename -- "$1")
+	extension="${filename##*.}"
+	basefilename="${filename%.*}"
+
+	listfiles=$(findhere.sh "$filename")
+	chosenfilepath=""
+	if [ -z "$listfiles" ]
+	then 
+		echo "$(basename -- $0): $1: No such file or directory"
+	else
+		if (( $(grep -c . <<<"$listfiles") > 1 )); then
+			echo >&2 "---> Multiple candidates for \"$filename\", please choose one:"
+			select i in $listfiles; do chosenfilepath=$i; break; done
+		else
+			chosenfilepath=$listfiles
+			echo >&2 "---> Single candidate found:"
+		fi
+		echo >&2 "chosen file path: $chosenfilepath"
+		echo >&2 -n "creating ---> "
+		path="tests/"$(dirname $chosenfilepath)/"test_"$basefilename.$extension
+		echo >&2 "$path"
+		echo "$path"
+		mkdir -p $(dirname -- $path)
+		touch "$path"
+		cmakelistemplate=tests/doc/template
+		echo >> $(dirname -- $path)/CMakeLists.txt
+		cat $cmakelistemplate/CMakeLists.txt | sed 's/template/'"$basefilename"'/g' >> $(dirname -- $path)/CMakeLists.txt
+	fi
+fi
