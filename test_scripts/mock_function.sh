cc3f92c38078c6ecbbb2e7623f74f79e38698a5f
isaadi <isaadi@kinova.ca>
2022-09-22 20:26:33 +0100

    added check for wrapper function before appending new one with mock_function.sh

--- test_scripts/mock_function.sh
+++ test_scripts/mock_function.sh
@@ -1,3 +1,5 @@
+# 
+# TODO: add wrapped function name to MOCKS in add_mocked_test() in the corresponding CMakeLists.txt
 if [ -z "$1" ]
 then
 	echo "No arguments were given"
@@ -7,25 +9,29 @@ else
 		if [ "$(grep $1 $file)" ]
 		then
 			proto=$(cat $file | regex.py '(^\s*(?:(?:inline|static)\s+){0,2}(?!else|typedef|return)\n*\w+\n*\s*\**\s*('$1')\s*\([^0;{]+\)\s*\{)' -)
-			# proto=$(cat $file | regex.py '(^\s*(?:(?:inline|static)\s+){0,2}(?!else|typedef|return)\n*\w+\n*\s+\*?\s*('$1')\s*\([^0;{]+\)\s*\{)' -)
 			if [ "$proto" ]
 			then
 				echo "$file:";
 				mock_file=$(create_mock_file.sh "$file");
 				if [ "$mock_file" ]
 				then
-					return_type=$(cat $file | regex.py '^\s*(?:(?:inline|static)\s+){0,2}(?!else|typedef|return)\n*(\w+)\n*\s+\**\s*('$1')\s*\([^0;{]+\)\s*\{' -)
-					# echo "file=$file"
-					# echo "rt=$return_type"
-					wrap_proto=$(echo "$proto" | sed 's/'"$1"'/'__wrap_"$1"'/g')
-					echo -e '\n'"$wrap_proto" >> "$mock_file" ;
-					if [ "$return_type" != "void" ]
+					if [ -z "$(cat $mock_file | regex.py '(^\s*(?:(?:inline|static)\s+){0,2}(?!else|typedef|return)\n*\w+\n*\s*\**\s*('__wrap_$1')\s*\([^0;{]+\)\s*\{)' -)" ]
 					then
-						echo -e '\treturn ('"$return_type"')mock();' >> $mock_file;
+						return_type=$(cat $file | regex.py '^\s*(?:(?:inline|static)\s+){0,2}(?!else|typedef|return)\n*(\w+)\n*\s+\**\s*('$1')\s*\([^0;{]+\)\s*\{' -)
+						# echo "file=$file"
+						# echo "rt=$return_type"
+						wrap_proto=$(echo "$proto" | sed 's/'"$1"'/'__wrap_"$1"'/g')
+						echo -e '\n'"$wrap_proto" >> "$mock_file" ;
+						if [ "$return_type" != "void" ]
+						then
+							echo -e '\treturn ('"$return_type"')mock();' >> $mock_file;
+						else
+							echo >> $mock_file;
+						fi
+						echo -e '}' >> $mock_file;
 					else
-						echo >> $mock_file;
+						echo 'wrapper for '"$1"' already exists!!'
 					fi
-					echo -e '}' >> $mock_file;
 					break;
 				fi
 			fi
