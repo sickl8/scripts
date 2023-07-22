abe42b1fd9f01d4e07fb0e7365545003bc0f714a
Ilyass Saadi <isaadi@e1r7p6.1337.ma>
2023-07-22 16:18:49 +0100

    i like dark mode now

--- set_defaults.sh
+++ set_defaults.sh
@@ -8,4 +8,4 @@ dfw ApplePressAndHoldEnabled -bool false
 dfw com.apple.swipescrolldirection -bool false
 dfw InitialKeyRepeat -int 15
 dfw KeyRepeat -int 2
-# dfw AppleInterfaceStyle -string Dark
+dfw AppleInterfaceStyle -string Dark
